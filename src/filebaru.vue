057cd5e1e74780333d7b7306af76fb8dbfcfa308
atheo810 <atheos810@gmail.com>
2023-10-30 10:15:36 +0800

    feat: parsing data

--- src/filebaru.vue
+++ src/filebaru.vue
@@ -1,9 +1,15 @@
 <template>
     <h1>Hallo disini saya mencoba membuat file vue</h1>
+    <p>{{ email }}</p>
 </template>
 
 <script>
 export default {
-    name: "filebaru"
+    name: "filebaru",
+    data(){
+        return{
+            email: "belajar@gmail.com"
+        }
+    }
 }
 </script>
